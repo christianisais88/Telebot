6984721de4a1392d79820509840d3e7f06c388f5
christianisais88 <christian.isais@gmail.com>
2024-08-07 12:35:11 -0600

    Update payments_example.py
    added my tokens

--- examples/payments_example.py
+++ examples/payments_example.py
@@ -1,13 +1,13 @@
 import telebot
 from telebot.types import LabeledPrice, ShippingOption
 
-token = '1234567890:AAAABBBBCCCCDDDDeeeeFFFFgggGHHHH'
-provider_token = '1234567890:TEST:AAAABBBBCCCCDDDD'  # @BotFather -> Bot Settings -> Payments
+token = '7380507230:AAEFVX88s_XY_EAJYmO859OdvwsLYMJGpks'
+provider_token = '350862534:LIVE:MDYyODQ1OWZhYzA4'  # @BotFather -> Bot Settings -> Payments
 bot = telebot.TeleBot(token)
 
 # More about Payments: https://core.telegram.org/bots/payments
 
-prices = [LabeledPrice(label='Working Time Machine', amount=5750), LabeledPrice('Gift wrapping', 500)]
+prices = [LabeledPrice(label='Working Time Machine', amount=100), LabeledPrice('Gift wrapping', 25)]
 
 shipping_options = [
     ShippingOption(id='instant', title='WorldWide Teleporter').add_price(LabeledPrice('Teleporter', 1000)),
@@ -17,17 +17,17 @@ shipping_options = [
 @bot.message_handler(commands=['start'])
 def command_start(message):
     bot.send_message(message.chat.id,
-                     "Hello, I'm the demo merchant bot."
-                     " I can sell you a Time Machine."
+                     "Hello, I'm the chiriwillo seller"
+                     " I can sell you a Chiriwillo"
                      " Use /buy to order one, /terms for Terms and Conditions")
 
 
 @bot.message_handler(commands=['terms'])
 def command_terms(message):
     bot.send_message(message.chat.id,
-                     'Thank you for shopping with our demo bot. We hope you like your new time machine!\n'
-                     '1. If your time machine was not delivered on time, please rethink your concept of time and try again.\n'
-                     '2. If you find that your time machine is not working, kindly contact our future service workshops on Trappist-1e.'
+                     'Thank you for shopping with our chiriwillo warehouse. We hope you like yours'
+                     '1. If your chiriwillo was not delivered on time, please rethink your concept of time and try again.\n'
+                     '2. If you find that your chiriwillo is not working, kindly contact our future service workshops on Trappist-1e.'
                      ' They will be accessible anywhere between May 2075 and November 4000 C.E.\n'
                      '3. If you would like a refund, kindly apply for one yesterday and we will have sent it to you immediately.')
 
@@ -35,12 +35,11 @@ def command_terms(message):
 @bot.message_handler(commands=['buy'])
 def command_pay(message):
     bot.send_message(message.chat.id,
-                     "Real cards won't work with me, no money will be debited from your account."
-                     " Use this test card number to pay for your Time Machine: `4242 4242 4242 4242`"
+                     "all cards from all around the world are welcome"
                      "\n\nThis is your demo invoice:", parse_mode='Markdown')
     bot.send_invoice(
                      message.chat.id,  #chat_id
-                     'Working Time Machine', #title
+                     'Chiriwillo', #title
                      ' Want to visit your great-great-great-grandparents? Make a fortune at the races? Shake hands with Hammurabi and take a stroll in the Hanging Gardens? Order our Working Time Machine today!', #description
                      'HAPPY FRIDAYS COUPON', #invoice_payload
                      provider_token, #provider_token
